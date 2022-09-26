38a668e84c9a6e6d0c0955e5fee226ed45c1d308
Saul Ruiz <saulruiz730@gmail.com>
2022-09-26 12:08:15 -0500

    created weapon

---
+++ battlefield.py
@@ -0,0 +1,13 @@
+class Battlefield:
+    def __init__(self):
+        robot = ''
+        dinosaur = ''
+    
+    def run_game(self):
+        pass
+
+    def display_welcome(slef):
+        print('Ladies and gentleman, welcome to the 5th annual Robot vs Dinosaur battle to the death!!')
+    
+    def battle_phase(self):
+        
